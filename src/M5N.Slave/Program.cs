724d6158eabf175e928aad7a560750346b431f59
Sharp0802 <sharp0802.business@gmail.com>
2024-02-09 05:38:56 +0900

    $WIP

--- src/M5N.Slave/Program.cs
+++ src/M5N.Slave/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text;
+using M5N.Slave;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding  = Encoding.UTF8;
+
 Console.WriteLine("Hello, World!");
+
+Python.Call("test", "main", "test");
