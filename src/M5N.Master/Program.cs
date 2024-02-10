6f475dfc12deb624848a083839efe9b6b0406481
Sharp0802 <sharp0802.business@gmail.com>
2024-02-11 04:38:32 +0900

    Feat: Implement master-side argument processing logic

--- src/M5N.Master/Program.cs
+++ src/M5N.Master/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using System.Net;
+using System.Net.Sockets;
+using M5N.Master;
 
-Console.WriteLine("Hello, World!");
+if (args.Length != 1)
+{
+    Console.Error.WriteLine("Invalid syntax.");
+    return 1;
+}
+
+if (!ushort.TryParse(args[0], out var port))
+{
+    Console.Error.WriteLine("Invalid port number.");
+    return 1;
+}
+
+using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+{
+    socket.Bind(new IPEndPoint(IPAddress.Any, port));
+    socket.Listen(2);
+
+    using var user0 = new MasterChannel(socket);
+    using var user1 = new MasterChannel(socket);
+
+    Console.WriteLine("Game established.");
+    
+    var win = new Master(user0, user1).Run();
+    
+    Console.WriteLine($"Game closed. Player {win} win.");
+    
+    socket.Shutdown(SocketShutdown.Both);
+}
+
+return 0;
