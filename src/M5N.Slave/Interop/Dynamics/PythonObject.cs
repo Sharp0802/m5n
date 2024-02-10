551682b9f0154252ce0ade60949288256cf34591
Sharp0802 <sharp0802.business@gmail.com>
2024-02-11 03:27:47 +0900

    Feat: Use alias for `None`

--- src/M5N.Slave/Interop/Dynamics/PythonObject.cs
+++ src/M5N.Slave/Interop/Dynamics/PythonObject.cs
@@ -5,7 +5,9 @@ namespace M5N.Slave.Interop.Dynamics;
 public abstract partial class PythonObject : IPythonObject
 {
     [DllImport(InteropConfiguration.FnExports)]
-    public static extern IntPtr GetAddressOfNoneObject();
+    private static extern IntPtr GetAddressOfNoneObject();
+
+    public static IntPtr None => GetAddressOfNoneObject();
     
     public abstract Type   Type   { get; }
     public abstract IntPtr Handle { get; }
