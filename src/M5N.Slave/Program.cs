f726db931960508fa57e460661c8029b285df698
Sharp0802 <sharp0802.business@gmail.com>
2024-02-12 06:24:53 +0900

    Feat: Implement multi-backend cli

--- src/M5N.Slave/Program.cs
+++ src/M5N.Slave/Program.cs
@@ -1,47 +1,42 @@
-﻿using System.Net;
-using System.Runtime.InteropServices;
-using System.Text;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Text;using M5N.Interop;
+using M5N.Interop.NativeLibrary;
 using M5N.Interop.Python;
 using M5N.Slave;
 
 Console.OutputEncoding = Encoding.UTF8;
 Console.InputEncoding  = Encoding.UTF8;
 
-if (args.Length is not (2 or 3))
+if (args.Length < 1)
 {
-    Console.Error.WriteLine("Invalid syntax.");
+    Console.Error.WriteLine("E: Insufficient argument count.");
     return 1;
 }
 
-EndPoint ep;
-if (IPEndPoint.TryParse(args[0], out var ipEp))
-{
-    ep = ipEp;
-}
-else
-{
-    var trunc = args[0].Split(':');
-    if (trunc.Length != 2)
-    {
-        Console.Error.WriteLine("Invalid DNS format.");
-        return 1;
-    }
+var offset = 0;
 
-    if (!ushort.TryParse(trunc[1], out var port))
-    {
-        Console.Error.WriteLine("Invalid port number.");
-        return 1;
-    }
-
-    ep = new DnsEndPoint(trunc[0], port);
+if (!TryParseEndPoint(args[offset++], out var ep))
+{
+    Console.Error.WriteLine("E: Invalid endpoint syntax.");
+    return 1;
 }
 
-var root   = args[1];
-var module = args.Length == 3 ? args[2] : "main";
-
-using (var gil = new PythonInteropEngine(root))
+if (!TryLoadEngine(args, ref offset, out var engine))
 {
-    var slave = new Slave(gil.ImportModule(module));
+    Console.Error.WriteLine("E: Invalid engine syntax.");
+    return 1;
+}
+
+using (engine)
+{
+    if (!TryLoadModule(args, ref offset, engine, out var module))
+    {
+        Console.Error.WriteLine("E: Invalid module syntax.");
+        return 1;
+    }
+    
+    var       slave   = new Slave(module);
     using var channel = new SlaveChannel(ep, slave);
     Console.WriteLine("Connection established.");
     channel.Run();
@@ -49,3 +44,71 @@ using (var gil = new PythonInteropEngine(root))
 }
 
 return 0;
+
+static bool TryParseEndPoint(string src, [NotNullWhen(true)] out EndPoint? ep)
+{
+    if (IPEndPoint.TryParse(src, out var ipEp))
+    {
+        ep = ipEp;
+        return true;
+    }
+
+    var trunc = src.Split(':');
+    if (trunc.Length != 2)
+    {
+        ep = null;
+        return false;
+    }
+
+    if (!ushort.TryParse(trunc[1], out var port))
+    {
+        ep = null;
+        return false;
+    }
+
+    ep = new DnsEndPoint(trunc[0], port);
+
+    return true;
+}
+
+static bool TryLoadEngine(string[] args, ref int offset, [NotNullWhen(true)] out IInteropEngine? engine)
+{
+    if (args.Length - offset < 1)
+    {
+        engine = null;
+        return false;
+    }
+    
+    switch (args[offset++])
+    {
+        case "Python":
+            if (args.Length - offset < 1)
+                goto default;
+            engine = new PythonInteropEngine(args[offset++]);
+            return true;
+        case "C":
+            engine = new CInteropEngine();
+            return true;
+        default:
+            engine = null;
+            return false;
+    }
+}
+
+static bool TryLoadModule(
+    string[] args, 
+    ref int offset, 
+    IInteropEngine engine, 
+    [NotNullWhen(true)] out InteropModule? module)
+{
+    if (args.Length - offset < 1)
+    {
+        module = null;
+        return false;
+    }
+
+    var name = args[offset++];
+    module = engine.ImportModule(name);
+    
+    return true;
+}
