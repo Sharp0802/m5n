cffe9aa80721f6360de404973fb26005d308bc5b
Sharp0802 <sharp0802.business@gmail.com>
2024-02-12 05:17:10 +0900

    Feat: Implement python exception

---
+++ src/M5N.Interop.Python/PythonException.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.InteropServices;
+
+namespace M5N.Interop.Python;
+
+public class PythonException()
+    : ExternalException(PyMarshal.AsString(PyMarshal.ToString(PyMarshal.GetRaisedException())));
+    
