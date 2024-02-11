9e838b78589b91642b3b7195f46aa7a494714ad2
Sharp0802 <sharp0802.business@gmail.com>
2024-02-12 05:17:47 +0900

    Feat: Declare marshaller interfaces

---
+++ src/M5N.Interop.Python/Converters/ConverterBase.cs
@@ -0,0 +1,14 @@
+﻿namespace M5N.Interop.Python.Converters;
+
+public abstract class ConverterBase<T> : IConverter<T>
+{
+    public abstract bool CanDeserialize(IntPtr obj);
+
+    public virtual bool CanSerialize(object? obj) => obj is T;
+
+    IntPtr IConverter. Serialize(object?  obj) => Serialize((T?)obj);
+    object? IConverter.Deserialize(IntPtr obj) => Deserialize(obj);
+
+    public abstract T?     Deserialize(IntPtr obj);
+    public abstract IntPtr Serialize(T?       obj);
+}
---
+++ src/M5N.Interop.Python/Converters/IConverter.cs
@@ -0,0 +1,19 @@
+﻿namespace M5N.Interop.Python.Converters;
+
+public interface IConverter
+{
+    public bool CanDeserialize(IntPtr obj);
+
+    public bool CanSerialize(object? obj);
+
+    public object? Deserialize(IntPtr obj);
+
+    public IntPtr Serialize(object? obj);
+}
+
+public interface IConverter<T> : IConverter
+{
+    public new T? Deserialize(IntPtr obj);
+
+    public IntPtr Serialize(T? obj);
+}
