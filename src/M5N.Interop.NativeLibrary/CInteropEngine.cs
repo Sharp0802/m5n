793564b5223a5d0fb94656b188974ff63bb48dfc
Sharp0802 <sharp0802.business@gmail.com>
2024-02-12 06:24:38 +0900

    Feat: Implement C backend

---
+++ src/M5N.Interop.NativeLibrary/CInteropEngine.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+
+namespace M5N.Interop.NativeLibrary;
+
+public sealed class CInteropEngine : IInteropEngine
+{
+    private readonly Dictionary<string, CInteropModule> _modules = new();
+    
+    public string Name => $"CLR {RuntimeEnvironment.GetSystemVersion()}";
+
+    public void ConfigureModule(string name, Action<CInteropModule> config)
+    {
+        if (!_modules.TryGetValue(name, out var module))
+        {
+            module = new CInteropModule(System.Runtime.InteropServices.NativeLibrary.Load(name));
+            _modules.Add(name, module);
+        }
+
+        config(module);
+    }
+    
+    public dynamic ImportModule(string name)
+    {
+        if (!_modules.TryGetValue(name, out var module))
+            throw new KeyNotFoundException($"Module '{name}' not configured");
+
+        return module;
+    }
+    
+    public void Dispose()
+    {
+        foreach (var module in _modules.Values)
+            module.Dispose();
+    }
+}
---
+++ src/M5N.Interop.NativeLibrary/CInteropModule.cs
@@ -0,0 +1,58 @@
+﻿using System.Dynamic;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+
+namespace M5N.Interop.NativeLibrary;
+
+public sealed class CInteropModule : InteropModule, IDisposable
+{
+    private readonly Dictionary<string, Delegate>                          _delegates  = new();
+    private readonly Dictionary<string, (Type Signature, Type[] TypeArgs)> _signatures = new();
+    private readonly IntPtr                                                _module;
+
+    public CInteropModule(IntPtr module)
+    {
+        _module = module;
+    }
+    
+    public bool TryRegisterSignature(string name, params Type[] args)
+    {
+        var type = Expression.GetDelegateType(args);
+        return _signatures.TryAdd(NameResolvingPolicy.Resolve(name), (type, args));
+    }
+    
+    public override bool TryInvokeMember(InvokeMemberBinder binder, object?[]? args, out object? result)
+    {
+        args ??= Array.Empty<object?>();
+
+        var name = NameResolvingPolicy.Resolve(binder.Name);
+        
+        if (!_signatures.TryGetValue(name, out var signature))
+        {
+            result = null;
+            return false;
+        }
+
+        if (signature.TypeArgs.Length - 1 != args.Length)
+        {
+            result = null;
+            return false;
+        }
+
+        var fp = System.Runtime.InteropServices.NativeLibrary.GetExport(_module, name);
+
+        if (!_delegates.TryGetValue(name, out var func))
+        {
+            func = Marshal.GetDelegateForFunctionPointer(fp, signature.Signature);
+            _delegates.Add(name, func);
+        }
+
+        result = func.DynamicInvoke(args);
+        return true;
+    }
+
+    public void Dispose()
+    {
+        System.Runtime.InteropServices.NativeLibrary.Free(_module);
+    }
+}
