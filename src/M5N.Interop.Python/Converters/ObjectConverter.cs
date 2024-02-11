e68cb3b06ad63948f0c6eef3374aa4ebf81ba3af
Sharp0802 <sharp0802.business@gmail.com>
2024-02-12 05:18:32 +0900

    Feat: Implement tuple/generic marshaller

---
+++ src/M5N.Interop.Python/Converters/ObjectConverter.cs
@@ -0,0 +1,58 @@
+﻿namespace M5N.Interop.Python.Converters;
+
+public class ObjectConverter : ConverterBase<object>
+{
+    private static IEnumerable<IConverter> Converters =>
+    [
+        new StringConverter(),
+        
+        new IntegerConverter<byte>(),
+        new IntegerConverter<ushort>(),
+        new IntegerConverter<uint>(),
+        new IntegerConverter<nuint>(),
+        new IntegerConverter<ulong>(),
+        
+        new IntegerConverter<sbyte>(),
+        new IntegerConverter<short>(),
+        new IntegerConverter<int>(),
+        new IntegerConverter<nint>(),
+        new IntegerConverter<long>(),
+        
+        new TupleConverter()
+    ];
+
+    public static ObjectConverter Default { get; } = new();
+    
+    public static IntPtr NoneHandle { get; }
+
+    static ObjectConverter()
+    {
+        NoneHandle = PyMarshal.GetAddressOfNoneObject();
+    }
+
+    public override bool CanDeserialize(IntPtr obj)
+    {
+        return obj == NoneHandle || 
+               Converters.Any(cvt => cvt.CanDeserialize(obj));
+    }
+
+    public override bool CanSerialize(object? obj)
+    {
+        return obj is null || 
+               Converters.Any(cvt => cvt.CanSerialize(obj));
+    }
+
+    public override object? Deserialize(IntPtr obj)
+    {
+        return obj == NoneHandle 
+            ? null 
+            : Converters.First(cvt => cvt.CanDeserialize(obj)).Deserialize(obj);
+    }
+
+    public override IntPtr Serialize(object? obj)
+    {
+        return obj is null 
+            ? NoneHandle 
+            : Converters.First(cvt => cvt.CanSerialize(obj)).Serialize(obj);
+    }
+}
---
+++ src/M5N.Interop.Python/Converters/TupleConverter.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+namespace M5N.Interop.Python.Converters;
+
+public class TupleConverter : ConverterBase<IEnumerable<object?>>
+{
+    public override bool CanDeserialize(IntPtr obj)
+    {
+        if (!PyMarshal.IsSubclassOf(obj, PyMarshal.TPFlags.TupleSubclass))
+            return false;
+
+        var size = PyMarshal.TupleSizeOf(obj);
+        for (var i = 0; i < size; ++i)
+        {
+            var item = PyMarshal.GetItemFromTuple(obj, i);
+            if (!ObjectConverter.Default.CanDeserialize(item))
+                return false;
+        }
+
+        return true;
+    }
+
+    public override bool CanSerialize(object? obj)
+    {
+        if (obj is null)
+            return false;
+        if (!base.CanSerialize(obj))
+            return false;
+
+        return ((IEnumerable)obj).OfType<object>().All(item => ObjectConverter.Default.CanSerialize(item));
+    }
+
+    public override IEnumerable<object?> Deserialize(IntPtr obj)
+    {
+        var size = PyMarshal.TupleSizeOf(obj);
+        for (var i = 0; i < size; ++i)
+        {
+            var item = PyMarshal.GetItemFromTuple(obj, i);
+            if (!ObjectConverter.Default.CanDeserialize(item))
+                throw new InvalidCastException();
+
+            yield return ObjectConverter.Default.Deserialize(item);
+        }
+    }
+
+    public override IntPtr Serialize(IEnumerable<object?>? obj)
+    {
+        ArgumentNullException.ThrowIfNull(obj);
+        
+        var arr = obj as object?[] ?? obj.ToArray();
+
+        var tuple = PyMarshal.NewTuple(arr.Length);
+        for (var i = 0; i < arr.Length; ++i)
+        {
+            var item = arr[i];
+            if (item is not null && !ObjectConverter.Default.CanSerialize(item))
+                throw new InvalidCastException();
+
+            PyMarshal.SetItemOfTuple(tuple, i, ObjectConverter.Default.Serialize(item));
+        }
+
+        return tuple;
+    }
+}
