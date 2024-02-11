cc53a37223daf4efcbd5a79be2ed4ca690d1d2a0
Sharp0802 <sharp0802.business@gmail.com>
2024-02-12 05:18:11 +0900

    Feat: Implement string marshaller

---
+++ src/M5N.Interop.Python/Converters/StringConverter.cs
@@ -0,0 +1,20 @@
+﻿namespace M5N.Interop.Python.Converters;
+
+public class StringConverter : ConverterBase<string>
+{
+    public override bool CanDeserialize(IntPtr obj)
+    {
+        return PyMarshal.IsSubclassOf(obj, PyMarshal.TPFlags.UnicodeSubclass);
+    }
+
+    public override string Deserialize(IntPtr obj)
+    {
+        return PyMarshal.AsString(obj);
+    }
+
+    public override IntPtr Serialize(string? obj)
+    {
+        ArgumentNullException.ThrowIfNull(obj);
+        return PyMarshal.FromString(obj);
+    }
+}
