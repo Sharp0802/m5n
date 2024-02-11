6f97edc2c4c48c54db8176f0e0dff096c4b97652
Sharp0802 <sharp0802.business@gmail.com>
2024-02-12 05:17:59 +0900

    Feat: Implement integer marshaller

---
+++ src/M5N.Interop.Python/Converters/IntegerConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+namespace M5N.Interop.Python.Converters;
+
+public class IntegerConverter<T> : ConverterBase<T> where T : IBinaryInteger<T>
+{
+    static IntegerConverter()
+    {
+        if (T.AllBitsSet.GetByteCount() > sizeof(long))
+            throw new InsufficientMemoryException("Type size should be less or equal than Int64.");
+    }
+    
+    
+    public override bool CanDeserialize(IntPtr obj)
+    {
+        return PyMarshal.IsSubclassOf(obj, PyMarshal.TPFlags.LongSubclass);
+    }
+
+    public override T Deserialize(IntPtr obj)
+    {
+        return T.CreateChecked(PyMarshal.AsInt64(obj));
+    }
+
+    public override IntPtr Serialize(T? obj)
+    {
+        if (obj is null)
+            throw new ArgumentNullException(nameof(obj));
+        return PyMarshal.FromInt64(long.CreateChecked(obj));
+    }
+}
