ed043199cd5386543d5f4f2ec75a86a354f108be
Sharp0802 <sharp0802.business@gmail.com>
2024-02-12 05:19:04 +0900

    Feat: Adopt python marshallers to DLR interfaces

---
+++ src/M5N.Interop.Python/PythonInteropEngine.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.Runtime.InteropServices;
+
+namespace M5N.Interop.Python;
+
+public sealed class PythonInteropEngine : IInteropEngine
+{
+    private static readonly object Lock = new();
+    private static          bool   _init;
+    
+    public PythonInteropEngine(string directory)
+    {
+        lock (Lock)
+        {
+            if (_init)
+                throw new DuplicateNameException();
+            _init = true;
+            
+            PyMarshal.Initialize();
+
+            var r = PyMarshal.RunSimpleString($"import sys;sys.path.append('{directory}')");
+            if (r != 0)
+                throw new ExternalException("Couldn't execute python code.", r);
+        }
+    }
+
+    public string Name { get; } = "cpython 3.11";
+    
+    public dynamic ImportModule(string name)
+    {
+        ObjectDisposedException.ThrowIf(!_init, this);
+        return new PythonInteropModule(name);
+    }
+
+    public void Dispose()
+    {
+        lock (Lock)
+        {
+            if (_init)
+                PyMarshal.Uninitialize();
+            _init = false;
+        }
+    }
+}
---
+++ src/M5N.Interop.Python/PythonInteropModule.cs
@@ -0,0 +1,48 @@
+﻿using System.Dynamic;
+using M5N.Interop.Python.Converters;
+
+namespace M5N.Interop.Python;
+
+public class PythonInteropModule : InteropModule
+{
+    public override INameResolvingPolicy NameResolvingPolicy { get; set; } = new SnakeCaseNameResolvingPolicy();
+
+    private readonly IntPtr                     _module;
+    private readonly Dictionary<string, IntPtr> _func = new();
+
+    internal PythonInteropModule(string name)
+    {
+        _module = PyMarshal.ImportModule(name);
+        if (_module == IntPtr.Zero)
+            throw new PythonException();
+    }
+
+    public override bool TryInvokeMember(InvokeMemberBinder binder, object?[]? args, out object? result)
+    {
+        var name = NameResolvingPolicy.Resolve(binder.Name);
+        
+        if (!_func.TryGetValue(name, out var func))
+        {
+            func = PyMarshal.GetAttrString(_module, name);
+            _func.Add(name, func);
+        }
+
+        if (func == IntPtr.Zero)
+        {
+            result = null;
+            return false;
+        }
+
+        var tuple = ObjectConverter.Default.Serialize(args);
+        var obj = PyMarshal.CallObject(func, tuple);
+
+        if (!ObjectConverter.Default.CanDeserialize(obj))
+        {
+            result = null;
+            return false;
+        }
+        
+        result = ObjectConverter.Default.Deserialize(obj);
+        return true;
+    }
+}
