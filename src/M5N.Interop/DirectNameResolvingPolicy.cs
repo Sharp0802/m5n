43a899a4b4a8c5ae16e72c0b5bcd3767d6a9de03
Sharp0802 <sharp0802.business@gmail.com>
2024-02-12 05:16:11 +0900

    Feat: Implement name-resolvers

---
+++ src/M5N.Interop/DirectNameResolvingPolicy.cs
@@ -0,0 +1,8 @@
+﻿namespace M5N.Interop;
+
+public class DirectNameResolvingPolicy : INameResolvingPolicy
+{
+    public static INameResolvingPolicy Default => new DirectNameResolvingPolicy();
+    
+    public string Resolve(string source) => source;
+}
---
+++ src/M5N.Interop/SnakeCaseNameResolvingPolicy.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace M5N.Interop;
+
+public class SnakeCaseNameResolvingPolicy : INameResolvingPolicy
+{
+    public static INameResolvingPolicy Default { get; } = new SnakeCaseNameResolvingPolicy();
+
+    public string Resolve(string source)
+    {
+        var builder = new StringBuilder(source.Length * 2);
+        for (var i = 0; i < source.Length; ++i)
+        {
+            if (char.IsUpper(source[i]))
+            {
+                if (i != 0 && source[i - 1] != '_')
+                    builder.Append('_');
+                builder.Append(char.ToLowerInvariant(source[i]));
+            }
+            else
+            {
+                builder.Append(source[i]);
+            }
+        }
+
+        return builder.ToString();
+    }
+}
