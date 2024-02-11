63129fa1ebbe1a4cf7e43ae58a906279ce12694c
Sharp0802 <sharp0802.business@gmail.com>
2024-02-12 05:14:05 +0900

    Refactor: Remove legacy codes to use dynamics rather than verbose delegates

--- src/M5N.Slave/Interop/Converters/IValueConverter.cs
+++
@@ -1,9 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-namespace M5N.Slave.Interop.Converters;
-
-public interface IValueConverter<T>
-{
-    public static abstract IntPtr Serialize(T        cs);
-    public static abstract bool   Deserialize(IntPtr py, [NotNullWhen(true)] out T? result);
-}
--- src/M5N.Slave/Interop/InteropConfiguration.cs
+++
@@ -1,8 +0,0 @@
-namespace M5N.Slave.Interop;
-
-public static class InteropConfiguration
-{
-    internal const string Python = "python3.11";
-
-    internal const string FnExports = "pyexport";
-}
