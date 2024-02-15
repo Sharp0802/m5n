e21abc06c68c65c03af46f78efe883bcc609c740
Sharp0802 <sharp0802.business@gmail.com>
2024-02-16 00:18:11 +0900

    Feat: Create endpoint dialog

---
+++ src/M5N.Desktop/EndPointWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace M5N.Desktop;
+
+public partial class EndPointWindow
+{
+    public EndPointWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Connect(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    public static string Inquiry()
+    {
+        var wnd = new EndPointWindow();
+        wnd.ShowDialog();
+        return wnd.EndpointBox.Text;
+    }
+}
